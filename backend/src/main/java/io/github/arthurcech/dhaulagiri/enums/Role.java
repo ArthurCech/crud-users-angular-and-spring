e9028efcd584ff25b3694bd2cb2b1448e5cacae4
Arthur Cech Francisco <arthurcech@gmail.com>
2023-01-04 12:21:52 -0300

    feat: add Role enum and authorities

---
+++ backend/src/main/java/io/github/arthurcech/dhaulagiri/enums/Role.java
@@ -0,0 +1,20 @@
+package io.github.arthurcech.dhaulagiri.enums;
+
+import static io.github.arthurcech.dhaulagiri.constants.AuthorityConstant.*;
+
+public enum Role {
+
+	ROLE_USER(USER_AUTHORITIES), ROLE_HR(HR_AUTHORITIES), ROLE_MANAGER(MANAGER_AUTHORITIES),
+	ROLE_ADMIN(ADMIN_AUTHORITIES), ROLE_SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES);
+
+	private String[] authorities;
+
+	Role(String... authorities) {
+		this.authorities = authorities;
+	}
+
+	public String[] getAuthorities() {
+		return authorities;
+	}
+
+}
