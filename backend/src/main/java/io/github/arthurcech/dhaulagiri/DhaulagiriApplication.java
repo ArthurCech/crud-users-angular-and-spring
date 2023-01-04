758329c3a5f4eaacc4518fe191dd26b29417f535
Arthur Cech Francisco <arthurcech@gmail.com>
2023-01-04 09:13:57 -0300

    Initial commit (create backend)

---
+++ backend/src/main/java/io/github/arthurcech/dhaulagiri/DhaulagiriApplication.java
@@ -0,0 +1,13 @@
+package io.github.arthurcech.dhaulagiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhaulagiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhaulagiriApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/arthurcech/dhaulagiri/DhaulagiriApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.arthurcech.dhaulagiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhaulagiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
