46911c0ba2037a233b466029eb1334d6b11bf825
Arthur Cech Francisco <arthurcech@gmail.com>
2023-01-05 08:08:08 -0300

    feat: create folder when app starts

--- backend/src/main/java/io/github/arthurcech/dhaulagiri/DhaulagiriApplication.java
+++ backend/src/main/java/io/github/arthurcech/dhaulagiri/DhaulagiriApplication.java
@@ -1,13 +1,18 @@
 package io.github.arthurcech.dhaulagiri;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.arthurcech.dhaulagiri.constants.FileConstant;
 
 @SpringBootApplication
 public class DhaulagiriApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DhaulagiriApplication.class, args);
+		new File(FileConstant.USER_FOLDER).mkdirs();
 	}
 
 }
