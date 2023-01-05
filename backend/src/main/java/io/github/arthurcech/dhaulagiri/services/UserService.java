87d58f620ccf7061b4d6686527a0d21707ec7933
Arthur Cech Francisco <arthurcech@gmail.com>
2023-01-05 08:09:59 -0300

    feat: delete, update, update image, add new User

--- backend/src/main/java/io/github/arthurcech/dhaulagiri/services/UserService.java
+++ backend/src/main/java/io/github/arthurcech/dhaulagiri/services/UserService.java
@@ -1,7 +1,10 @@
 package io.github.arthurcech.dhaulagiri.services;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import io.github.arthurcech.dhaulagiri.entities.User;
 
@@ -15,18 +18,18 @@ public interface UserService {
 
 	Optional<User> findByEmail(String email);
 
-//	User addNewUser(String firstName, String lastName, String username, String email, String role,
-//			boolean isNonLocked, boolean isActive, MultipartFile profileImage);
-//
-//	User updateUser(String currentUsername, String newFirstName, String newLastName,
-//			String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive,
-//			MultipartFile profileImage);
-//
-//	void deleteUser(String username);
-//
-//	void resetPassword(String email);
-//
-//	User updateProfileImage(String username, MultipartFile profileImage);
+	User addNewUser(String firstName, String lastName, String username, String email, String role,
+			boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws IOException;
+
+	User updateUser(String currentUsername, String newFirstName, String newLastName,
+			String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive,
+			MultipartFile profileImage) throws IOException;
+
+	void deleteUser(String username) throws IOException;
+
+	void resetPassword(String email);
+
+	User updateProfileImage(String username, MultipartFile profileImage) throws IOException;
 
 	void setLastLoginDate(User user);
 
--- backend/src/main/java/io/github/arthurcech/dhaulagiri/services/impl/UserServiceImpl.java
+++ backend/src/main/java/io/github/arthurcech/dhaulagiri/services/impl/UserServiceImpl.java
@@ -1,29 +1,51 @@
 package io.github.arthurcech.dhaulagiri.services.impl;
 
 import static io.github.arthurcech.dhaulagiri.constants.FileConstant.DEFAULT_USER_IMAGE_PATH;
+import static io.github.arthurcech.dhaulagiri.constants.FileConstant.DOT;
+import static io.github.arthurcech.dhaulagiri.constants.FileConstant.FORWARD_SLASH;
+import static io.github.arthurcech.dhaulagiri.constants.FileConstant.JPG_EXTENSION;
+import static io.github.arthurcech.dhaulagiri.constants.FileConstant.NOT_AN_IMAGE_FILE;
+import static io.github.arthurcech.dhaulagiri.constants.FileConstant.USER_FOLDER;
+import static io.github.arthurcech.dhaulagiri.constants.FileConstant.USER_IMAGE_PATH;
 import static io.github.arthurcech.dhaulagiri.constants.ServiceConstant.EMAIL_ALREADY_EXISTS;
 import static io.github.arthurcech.dhaulagiri.constants.ServiceConstant.USERNAME_ALREADY_EXISTS;
 import static io.github.arthurcech.dhaulagiri.constants.ServiceConstant.USER_NOT_FOUND;
+import static io.github.arthurcech.dhaulagiri.constants.ServiceConstant.USER_NOT_FOUND_BY_EMAIL;
 import static io.github.arthurcech.dhaulagiri.enums.Role.ROLE_USER;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.springframework.http.MediaType.IMAGE_GIF_VALUE;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.github.arthurcech.dhaulagiri.constants.ServiceConstant;
 import io.github.arthurcech.dhaulagiri.entities.User;
 import io.github.arthurcech.dhaulagiri.entities.UserPrincipal;
+import io.github.arthurcech.dhaulagiri.enums.Role;
 import io.github.arthurcech.dhaulagiri.exceptions.entities.EmailExistException;
+import io.github.arthurcech.dhaulagiri.exceptions.entities.EmailNotFoundException;
+import io.github.arthurcech.dhaulagiri.exceptions.entities.NotAnImageFileException;
 import io.github.arthurcech.dhaulagiri.exceptions.entities.UserNotFoundException;
 import io.github.arthurcech.dhaulagiri.exceptions.entities.UsernameExistException;
 import io.github.arthurcech.dhaulagiri.repositories.UserRepository;
@@ -101,6 +123,79 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 		return repository.findByEmail(email);
 	}
 
+	@Override
+	public User addNewUser(String firstName, String lastName, String username, String email,
+			String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage)
+			throws IOException {
+		validateUsernameAndEmail(null, username, email);
+
+		String password = generatePassword();
+
+		User user = new User.Builder().setUserId(generateUserId()).setFirstName(firstName)
+				.setLastName(lastName).setUsername(username).setEmail(email).setJoinDate(new Date())
+				.setPassword(encodePassword(password)).setActive(isActive).setNotLocked(isNonLocked)
+				.setRole(getRoleEnumName(role).name())
+				.setAuthorities(getRoleEnumName(role).getAuthorities())
+				.setProfileImageUrl(getTemporaryProfileImageUrl(username)).build();
+
+		repository.save(user);
+
+		saveProfileImage(user, profileImage);
+
+		return user;
+	}
+
+	@Override
+	public User updateUser(String currentUsername, String newFirstName, String newLastName,
+			String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive,
+			MultipartFile profileImage) throws IOException {
+		User currentUser = validateUsernameAndEmail(currentUsername, newUsername, newEmail);
+
+		currentUser.setFirstName(newFirstName);
+		currentUser.setLastName(newLastName);
+		currentUser.setUsername(newUsername);
+		currentUser.setEmail(newEmail);
+		currentUser.setActive(isActive);
+		currentUser.setNotLocked(isNonLocked);
+		currentUser.setRole(getRoleEnumName(role).name());
+		currentUser.setAuthorities(getRoleEnumName(role).getAuthorities());
+
+		repository.save(currentUser);
+
+		saveProfileImage(currentUser, profileImage);
+
+		return currentUser;
+	}
+
+	@Override
+	public void deleteUser(String username) throws IOException {
+		User user = repository.findByUsername(username)
+				.orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND.formatted(username)));
+		Path userFolder = Paths.get(USER_FOLDER + user.getUsername()).toAbsolutePath().normalize();
+		FileUtils.deleteDirectory(new File(userFolder.toString()));
+		repository.delete(user);
+	}
+
+	@Override
+	public void resetPassword(String email) {
+		User user = repository.findByEmail(email).orElseThrow(
+				() -> new EmailNotFoundException(USER_NOT_FOUND_BY_EMAIL.formatted(email)));
+
+		String password = generatePassword();
+		user.setPassword(encodePassword(password));
+
+		repository.save(user);
+
+		emailService.sendNewPasswordEmail(user.getFirstName(), password, user.getEmail());
+	}
+
+	@Override
+	public User updateProfileImage(String username, MultipartFile profileImage) throws IOException {
+		User user = validateUsernameAndEmail(username, null, null);
+		saveProfileImage(user, profileImage);
+		return user;
+	}
+
 	private User validateUsernameAndEmail(String currentUsername, String newUsername,
 			String newEmail) {
 		Optional<User> userByNewUsername = findByUsername(newUsername);
@@ -159,6 +254,46 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 			}
 		} else {
 			loginAttemptService.removeUserFromLoginCache(user.getUsername());
+		}
+	}
+
+	private Role getRoleEnumName(String role) {
+		return Role.valueOf(role.toUpperCase());
+	}
+
+	private void saveProfileImage(User user, MultipartFile profileImage) throws IOException {
+		if (profileImage != null) {
+			validateFileType(profileImage);
+
+			Path userFolder = Paths.get(USER_FOLDER + user.getUsername()).toAbsolutePath()
+					.normalize();
+
+			if (!Files.exists(userFolder)) {
+				Files.createDirectories(userFolder);
+			}
+
+			Files.deleteIfExists(Paths.get(userFolder + user.getUsername() + DOT + JPG_EXTENSION));
+
+			Files.copy(profileImage.getInputStream(),
+					userFolder.resolve(user.getUsername() + DOT + JPG_EXTENSION), REPLACE_EXISTING);
+
+			user.setProfileImageUrl(setProfileImageUrl(user.getUsername()));
+
+			repository.save(user);
+		}
+	}
+
+	private String setProfileImageUrl(String username) {
+		return ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path(USER_IMAGE_PATH + username + FORWARD_SLASH + username + DOT + JPG_EXTENSION)
+				.toUriString();
+	}
+
+	private void validateFileType(MultipartFile image) {
+		List<String> types = Arrays.asList(IMAGE_JPEG_VALUE, IMAGE_PNG_VALUE, IMAGE_GIF_VALUE);
+
+		if (!types.contains(image.getContentType())) {
+			throw new NotAnImageFileException(image.getOriginalFilename() + NOT_AN_IMAGE_FILE);
 		}
 	}
 
