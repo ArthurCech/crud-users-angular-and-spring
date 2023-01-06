0ab5473260bdc82e7349cc342cdba4d997f81aa3
Arthur Cech Francisco <arthurcech@gmail.com>
2023-01-06 12:48:22 -0300

    feat: add new users

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { NotificationType } from '../enum/notification-type.enum';
 import { User } from '../model/user.model';
@@ -20,6 +21,9 @@ export class UserComponent implements OnInit, OnDestroy {
 
   public users: User[];
   public selectedUser: User;
+
+  public fileName: string;
+  public profileImage: File;
 
   constructor(private userService: UserService,
     private notificationService: NotificationService) { }
@@ -60,6 +64,39 @@ export class UserComponent implements OnInit, OnDestroy {
   public onSelectUser(user: User): void {
     this.selectedUser = user;
     document.getElementById('openUserInfo').click();
+  }
+
+  public onProfileImageChange(event: Event): void {
+    const profileImage = (<HTMLInputElement>event.target).files[0];
+    this.fileName = profileImage.name;
+  }
+
+  public onSaveUser(): void {
+    document.getElementById('new-user-save').click();
+  }
+
+  public saveUser(userForm: NgForm): void {
+    const formData = this.userService.createUserFormDate(null, userForm.value, this.profileImage);
+    this.subscriptions.push(
+      this.userService.addUser(formData).subscribe(
+        {
+          next: (user: User) => {
+            document.getElementById('new-user-close').click();
+            this.getUsers(false);
+            userForm.reset();
+            this.sendNotification(NotificationType.SUCCESS,
+              `${user.firstName} ${user.lastName} added successfully`);
+          },
+          error: (err: HttpErrorResponse) => {
+            this.sendNotification(NotificationType.ERROR, err.error.message);
+          },
+          complete: () => {
+            this.profileImage = null;
+            this.fileName = null;
+          }
+        }
+      )
+    );
   }
 
   private sendNotification(type: NotificationType, message: string): void {
